27cce26a46b8e6cdc51dcd9925fc1947c2ff98b1
Gregory Bell <gregory.bell3@snhu.edu>
2022-11-01 14:43:04 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.playerCharacter = new System.Windows.Forms.PictureBox();
+            this.playerMoveTime = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCharacter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(323, 459);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 171);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "Wall";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(-47, 459);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(314, 10);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "Wall";
+            // 
+            // playerCharacter
+            // 
+            this.playerCharacter.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.playerCharacter.Location = new System.Drawing.Point(422, 561);
+            this.playerCharacter.Name = "playerCharacter";
+            this.playerCharacter.Size = new System.Drawing.Size(26, 49);
+            this.playerCharacter.TabIndex = 2;
+            this.playerCharacter.TabStop = false;
+            this.playerCharacter.Tag = "Player";
+            // 
+            // playerMoveTime
+            // 
+            this.playerMoveTime.Enabled = true;
+            this.playerMoveTime.Interval = 10;
+            this.playerMoveTime.Tick += new System.EventHandler(this.playerMoveTime_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(-60, 620);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(393, 10);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Wall";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.SaddleBrown;
+            this.pictureBox4.Location = new System.Drawing.Point(1, 475);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(316, 139);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "Roof";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 642);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.playerCharacter);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCharacter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer playerMoveTime;
+        public System.Windows.Forms.PictureBox playerCharacter;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool characterLeft = false;
+        bool characterRight = false;
+        bool characterDown = false;
+        bool characterUp = false;
+
+        int playerSpeed = 5;
+
+        
+
+
+        private void playerMoveTime_Tick(object sender, EventArgs e)
+        {
+            
+            //move character
+            if (characterLeft == true)
+            {
+                playerCharacter.Left -= playerSpeed;
+
+            }
+            if (characterRight == true)
+            {
+                playerCharacter.Left += playerSpeed;
+
+            }
+            if (characterUp == true)
+            {
+                playerCharacter.Top -= playerSpeed;
+
+            }
+            if (characterDown == true)
+            {
+                playerCharacter.Top += playerSpeed;
+
+            }//end of character movement
+
+            //Make walls solid
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if((string)x.Tag == "Wall")
+                    {
+                        if (playerCharacter.Bounds.IntersectsWith(x.Bounds) && characterLeft == true)
+                        {
+                            playerCharacter.Left += playerSpeed;
+                        }
+                        else if(playerCharacter.Bounds.IntersectsWith(x.Bounds) && characterRight == true)
+                        {
+                            playerCharacter.Left -= playerSpeed;
+                        }
+                        else if (playerCharacter.Bounds.IntersectsWith(x.Bounds) && characterUp == true)
+                        {
+                            playerCharacter.Top += playerSpeed;
+                        }
+                        else if (playerCharacter.Bounds.IntersectsWith(x.Bounds) && characterDown == true)
+                        {
+                            playerCharacter.Top -= playerSpeed;
+                        }
+
+                    }
+                }
+            }//end of walls
+
+            //Make roof invisible on entry of room
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if((string)x.Tag == "Roof")
+                    {
+                        if (playerCharacter.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            x.Visible = false;
+                        }
+                    }
+                }
+            }//end of hide roof
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "Roof")
+                    {
+                        if (!playerCharacter.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            x.Visible = true;
+                        }
+                    }
+                }
+            }//end of hide roof
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                characterLeft = true;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                characterRight = true;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                characterUp = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                characterDown = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                characterLeft = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                characterRight = false;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                characterUp = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                characterDown = false;
+            }
+
+            
+        }
+        private void RestartGame()
+        {
+            bool characterLeft = false;
+            bool characterRight = false;
+            bool characterDown = false;
+            bool characterUp = false;
+
+            int playerSpeed = 10;
+
+            playerCharacter.Left = 422;
+            playerCharacter.Top = 561;
+
+        }
+    }
+}
